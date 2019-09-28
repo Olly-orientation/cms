57dba8ff2bb23932b75f2e23509e411e5d7603f1
Olly_orientation <17779217937@163.com>
2019-09-28 15:38:55 +0800

    完成了一些表类名的设置，等待服务器

--- cms/settings.py
+++ cms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,6 +79,7 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        "db":"",
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
--- server/models.py
+++ server/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class article(models.Model):
+    articleId=models.AutoField(primary_key=True)
+    authorId=models.IntegerField(max_length=11)
+    columnId=models.IntegerField(max_length=11)
+    sourceId=models.IntegerField(max_length=11)
+    browserNum=models.IntegerField(max_length=11)
+    articleStatus=models.IntegerField(max_length=1)
+    heading=models.CharField()
+    modifydate=models.DateTimeField(auto_now=True)
+
+class articlecontent(models.Model):
+    contentId=models.AutoField(primary_key=True)
+    articleId=models.IntegerField(max_length=11)
+    contents=models.TextField()
+    imgUrl=models.CharField()
+
+class source(models.Model):
+    sourceId=models.AutoField(primary_key=True)
+    sourceName=models.CharField()
+
+class topics(models.Model):
+    topicId=models.AutoField(primary_key=True)
+    topicName=models.CharField()
+
+class menu(models.Model):
+    menuId=models.AutoField(primary_key=True)
+    menuName=models.CharField()
+    imgUrl=models.CharField()
+    cos=models.IntegerField(max_length=1)
+    status=models.IntegerField(max_length=1)
+
